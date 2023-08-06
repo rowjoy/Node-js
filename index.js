7456e42f30bfe02134daab4dde6adaedd8ee2228
NecJoy <joy@primotechltd.com>
2023-08-06 16:19:12 +0600

    Node - js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-unused-vars */
 console.log('Hello world');
+const name = require('lodash');
+
+console.log(name.last([1, 2, 3]));
+
+const getManList = require('./man');
+
+getManList.test();
+const data = getManList.test();
+console.log(getManList.manlists);
+console.log(name.capitalize('ADW'));
+console.log(name.isBuffer(false));
+console.log(name.sortedLastIndexBy([4, 12, 4]));
+console.log(name.sortedUniq([1, 4, 2, 5]));
+function checkData(_data) {
+    if (_data === 9) {
+        console.log('Wow nice');
+    } else if (_data === 10) {
+        console.log('Bad');
+    }
+}
+console.log(checkData());
+// 5 no
+// Users/nec/Documents/Learning/node-js/index.js
+// 22.00 minit
+// https://www.youtube.com/watch?v=6Yv3YXgPBJU
---
+++ man.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+const manlists = ['Shakib', 'Tamim', 'Hrody'];
+// console.log(manlists);
+function test() {
+    const a = 4;
+    const b = 5;
+    console.log(a + b);
+    return a + b;
+}
+
+const appPath = 'Users/nec/Documents/Learning/node-js/index.js';
+
+module.exports = {
+    manlists,
+    test,
+    appPath,
+};
