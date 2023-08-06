ae627059febeb6e7e5efb958eb0e53ed97e40366
NecJoy <joy@primotechltd.com>
2023-08-06 19:22:01 +0600

    JavaScprit

---
+++ javascript/operators_types.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-tabs */
+/*
+
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+String Operators
+Logical Operators
+Bitwise Operators
+Ternary Operators
+Type Operators
+
+ */
+
+// Arithmetic Operators
+//----------------------------------------
+/*
+
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Division Remainder),
+++	Increment
+--	Decrement
+
+*/
+
+// Assignment Operator
+//----------------------------
+/*
+
+Operator	Example	Same As
+ =	x = y	x = y
++=	x += y	x = x + y
+-=	x -= y	x = x - y
+*=	x *= y	x = x * y
+/=	x /= y	x = x / y
+%=	x %= y	x = x % y
+**=	x **= y	x = x ** y
+
+ */
+
+// Comprassion Operator
+//-----------------------------------------------
+/*
+
+==	equal to
+===	equal value and equal type
+!=	not equal
+!==	not equal value or not equal type
+>	greater than
+<	less than
+>=	greater than or equal to
+<=	less than or equal to
+?	ternary operator
+ */
+
+// Logical Operators
+//---------------------------------------------
+
+/*
+
+&&	logical and
+||	logical or
+!	logical not
+
+*/
+// Type Operators
+//----------------------------------------------
+/*
+
+typeof	Returns the type of a variable
+instanceof	Returns true if an object is an instance of an object type
+
+*/
